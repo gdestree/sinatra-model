78bd84e64288b930fc34d8854f5d255546b8e353
Gregor Destrée <email": null>
2016-06-08 08:07:26 -0400

    Template edits

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -23,3 +23,23 @@ get '/users/:id' do
   erb :'users/show'
 end
 
+get '/users/:id/edit' do
+  @user = User.find_by(id: params[:id])
+
+  if current_user.id == @user.id
+    erb :'/users/edit'
+  else
+    erb :'/users/errors'
+  end
+end
+
+put '/users/:id' do
+  @user.assign_attributes(params[:user])
+
+  if @user.save
+    redirect "users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'users/edit'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+  validates :first_name, presence: true
+  validates :username, presence: true, uniqueness: true
 
   # has_many
   # has_many
